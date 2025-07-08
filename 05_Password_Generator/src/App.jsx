079b07421417560c3fecd0b2204837e1c282d89b
milind55555 <hanchatemilind291@gmail.com>
2025-07-08 23:44:16 +0530

    feat: update Password Generator project with new password generation logic and UI enhancements

--- 05_Password_Generator/src/App.jsx
+++ 05_Password_Generator/src/App.jsx
@@ -1,24 +1,115 @@
-import { useState } from 'react'
+import { useState ,useCallback,useEffect, useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [length,setLength] =useState(8)
+  const [Length,setLength] =useState(8)
   const [numberAllowed,setNumberAllowed]=useState(false)
   const [CharAllowed,setCharAllowed]=useState(false)
   const [password,setPassword]=useState("")
+
+  // useRef hook to store the password generator function
+  const passwordRef = useRef(null)
   
-  const passwordGenerator= ()=>{
+  const passwordGenerator= useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed ) str += "0123456789"
+    if(CharAllowed) str += "!@#$%^&*()[]{}"
+
+    for(let i=1;i <= Length ; i++){
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass +=str.charAt(char)
+
+    }
+
+    setPassword(pass)
+
     
-  }
+  } ,[length,numberAllowed,CharAllowed,setPassword])
+
+  const copyPasswordToClickboard =  useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 99999); // For mobile devices
+    window.navigator.clipboard.writeText(password)
+
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[Length,numberAllowed,CharAllowed,passwordGenerator])
 
   return (
   <>
-    <h1 
-    className='text-4xl text-center'>
-      Password Generator
-    </h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-400 bg-gray-800'>
+       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <h1 className="text-gray">Password Generator</h1>
+        <input 
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password'
+        readOnly
+        ref={passwordRef}
+         />
+         <button
+         onClick={copyPasswordToClickboard}
+         >
+        copy
+       </button>
+
+       </div>
+
+       <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input
+             type="range"
+             min={6}
+             max={100}
+             className='cursor-pointer'
+             onChange={(e)=>setLength(e.target.value)}
+            
+            />
+            <label>Length:{Length}</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={()=>{
+                setNumberAllowed((prev)=> !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+
+
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={CharAllowed}
+              id="charInput"
+              onChange={()=>{
+                setCharAllowed((prev)=> !prev);
+              }}
+            />
+            <label htmlFor="charInput">Characters</label>
+          </div>
+
+
+
+
+
+
+       </div>
+       
+       
+       </div>
+
   </>
   )
 }
--- 05_Password_Generator/tailwind.config.js
+++ 05_Password_Generator/tailwind.config.js
@@ -1,4 +1,3 @@
-// filepath: d:\React_Learning_From_Scratch\React_Learning_\05_Password_Generator\tailwind.config.js
 module.exports = {
   content: [
     "./index.html",
